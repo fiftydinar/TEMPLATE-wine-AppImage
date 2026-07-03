888416cc6cfd8a0132ba254170efe10883441504
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-07-03 18:56:44 +0200

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,11 +11,12 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/bcf6aa9582f676e1c93d0022319e6055cd1f2de2/Papirus/64x64/apps/wine.svg
 export DESKTOP=/usr/share/applications/wine.desktop
 export APPNAME=Rufus
+export MAIN_BIN=rufus
 # Wine app deployment variables, similar to 'quick-sharun'
 export WINEPREFIX=/tmp/wine
 export WINE_MAIN_BIN=rufus
 export STRACE_BINARY=wine
-export STRACE_FLAGS=/tmp/rufus
+export STRACE_FLAGS=/tmp/rufus.exe
 #export WINE_STRACE_TIME=15
 #export WINE_STRACE_BINARY=/PATH/TO/EXE_HERE
 #export WINE_STRACE_FLAGS='--disable-gpu'
@@ -29,18 +30,15 @@ export STRACE_FLAGS=/tmp/rufus
 
 # Download and install Windows app to ./AppDir/share/WINE_MAIN_BIN folder (portable version is preferred)
 mkdir -p ./AppDir/share/rufus/
-wget https://github.com/pbatard/rufus/releases/download/v4.15/rufus-4.15p.exe -O ./AppDir/share/rufus/rufus
+wget https://github.com/pbatard/rufus/releases/download/v4.15/rufus-4.15p.exe -O ./AppDir/share/rufus/rufus.exe
+chmod +x ./AppDir/share/rufus/rufus.exe
 
 # Trace wine app from path above and cleanup unneded wine dependencies
-cp -v ./AppDir/share/rufus/rufus /tmp/rufus
-wine-strace /tmp/rufus
+cp -v ./AppDir/share/rufus/rufus.exe /tmp/rufus.exe
+wine-strace /tmp/rufus.exe
 
 # Deploy dependencies (wine bin + libs, wget and zenity are basic ones)
-quick-sharun                   \
-	/usr/bin/wine*             \
-	/usr/lib/wine              \
-	/usr/bin/wget              \
-	/usr/bin/zenity
+quick-sharun /usr/bin/wine
 
 wine-fixes-after-deploy
 
